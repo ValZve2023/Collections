eead20b1ea7ee8fd98651b892dfc4cd8b28a01c4
ValZve2023 <frau.zvereva2009@yandex.ru>
2023-11-13 20:11:12 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/CollectionsHomework/CollectionsHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.skypro.CollectionsHomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsHomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/skypro/CollectionsHomework/CollectionsHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.CollectionsHomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsHomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
