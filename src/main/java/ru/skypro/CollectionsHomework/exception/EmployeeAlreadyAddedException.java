1929cd205f59a4115436c852fa7c3b5f82be1f47
ValZve2023 <frau.zvereva2009@yandex.ru>
2023-12-19 21:26:13 +0300

    libraries

--- src/main/java/ru/skypro/CollectionsHomework/exception/EmployeeAlreadyAddedException.java
+++ src/main/java/ru/skypro/CollectionsHomework/exception/EmployeeAlreadyAddedException.java
@@ -3,7 +3,7 @@ package ru.skypro.CollectionsHomework.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.CREATED)
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public class EmployeeAlreadyAddedException extends RuntimeException {
     public EmployeeAlreadyAddedException(String message) {
         super(message);
